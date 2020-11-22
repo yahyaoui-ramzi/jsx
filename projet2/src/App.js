cc1183cb0ca599825cbcdba28f1906c487a663e8
unknown <ramzi.yahyaoui89@gmail.com>
2020-11-22 21:43:25 +0100

    firestcommit

---
+++ projet2/src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+import theImage from './pic.jpg' ;
+import './style.css' ;
+import myVideo from './inter.mp4';
+
+
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1 class="title red">Yahyaoui Ramzi</h1>
+      
+      <img src  ={theImage} className = "pic"/>
+      
+
+      <img  src={process. env. PUBLIC_URL+ './pic2.Jpg'} className = "thepic" />
+
+      <video src= {myVideo} type="inter/.mp4" />;
+
+
+
+
+
+    </div>
+    
+    
+    
+  );
+}
+
+export default App;
